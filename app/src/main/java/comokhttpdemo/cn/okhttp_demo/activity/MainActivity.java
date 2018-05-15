b910d66b4063a21d38bb817e3ca90975b8017647
fengtime <fengtime1@sina.com>
2018-05-15 16:40:55 +0800

    add feature-1 on feature-1 branch

--- app/src/main/java/comokhttpdemo/cn/okhttp_demo/activity/MainActivity.java
+++ app/src/main/java/comokhttpdemo/cn/okhttp_demo/activity/MainActivity.java
@@ -2,6 +2,7 @@ package comokhttpdemo.cn.okhttp_demo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import comokhttpdemo.cn.okhttp_demo.R;
 
@@ -11,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * feature-1
+     * display HelloWorld
+     * @param view
+     */
+    public void feature_1(View view){
+
     }
 
     @Override
