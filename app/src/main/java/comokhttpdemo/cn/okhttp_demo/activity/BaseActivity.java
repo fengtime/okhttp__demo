cd1d53199878e61cb607e7dfd9ea092ad2f3f3de
fengtime <fengtime1@sina.com>
2018-05-16 14:31:25 +0800

    master baseactivity test

--- app/src/main/java/comokhttpdemo/cn/okhttp_demo/activity/BaseActivity.java
+++ app/src/main/java/comokhttpdemo/cn/okhttp_demo/activity/BaseActivity.java
@@ -3,19 +3,23 @@ package comokhttpdemo.cn.okhttp_demo.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 /**
  * Created by orange on 2018/5/16.
  */
 
-public class BaseActivity extends Activity{
+public abstract class BaseActivity extends Activity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    public int layoutid(){
-        return 0;
+    public abstract int getLayoutid();
+
+    public <T extends View> T findViewByIds(int res) {
+        View view = findViewById(res);
+        return (T) view;
     }
 
     @Override
